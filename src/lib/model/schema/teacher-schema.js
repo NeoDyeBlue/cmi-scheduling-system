1c7c3858529aecf1af0e962676ec9594736ee70b
Adam <adammarcaida06261996@gmail.com>
2023-03-20 11:35:50 +0800

    schema

--- src/lib/model/schema/teacher-schema.js
+++ src/lib/model/schema/teacher-schema.js
@@ -1,11 +1,12 @@
 import mongoose from 'mongoose';
 
+const time = new mongoose.Schema({
+  start: { type: String },
+  end: { type: String },
+});
 const preferredDaysSchema = new mongoose.Schema({
   day: { type: String },
-  time: {
-    start: { type: String },
-    end: { type: String },
-  },
+  time: [time],
 });
 const assignedSubjectsSchema = new mongoose.Schema({
   subject: { type: mongoose.Types.ObjectId, ref: 'Subject', required: true },
@@ -13,6 +14,7 @@ const assignedSubjectsSchema = new mongoose.Schema({
   year: { type: String },
   section: { type: String },
 });
+
 const teacher = new mongoose.Schema({
   firstName: { type: String, required: true },
   middleName: { type: String },
