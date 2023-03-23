2f80d944e847028fdcee390c869b2a02b57823ef
NeoDyeBlue <57549644+NeoDyeBlue@users.noreply.github.com>
2023-03-23 12:25:16 +0800

    Update subject-schema.js

--- src/lib/model/schema/subject-schema.js
+++ src/lib/model/schema/subject-schema.js
@@ -6,7 +6,7 @@ const subject = new mongoose.Schema({
   code: { type: String, unique: true },
   name: { type: String, required: true },
   units: { type: Number, required: true },
-  type: { type: String, enum: ['SHS', 'College'] },
+  type: { type: String, enum: ['shs', 'college'] },
   semester: { type: String, required: true, enum: ['1', '1'] },
   assignedTeachers: [
     new mongoose.Schema({
