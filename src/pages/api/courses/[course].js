ad72f42dfcd62005edc81018876cc93156b32d6f
Adam <adammarcaida06261996@gmail.com>
2023-03-28 10:48:26 +0800

    update course, room api

--- src/pages/api/courses/[course].js
+++ src/pages/api/courses/[course].js
@@ -1,25 +1,20 @@
 import Course from '@/lib/model/data-access/Course';
 import { errorResponse, successResponse } from '@/utils/response.utils';
-
+import Room from '@/lib/model/data-access/Room';
 export const handler = async (req, res) => {
   const { p } = req.query;
   if (req.method === 'GET' && p === 'draggable') {
     try {
-      const {
-        semester,
-        year,
-        section,
-        course: courseCode,
-        room: roomCode,
-      } = req.query;
+      const { semester, year, section, course: courseCode } = req.query;
+      const roomSchedules = await Room.getAllRoomSchedules();
+
       const data = await Course.getCourseSubjectTeachers({
         courseCode: courseCode.toLowerCase(),
         semester,
         year,
         section,
-        roomCode,
       });
-
+      data[0]['rooms'] = roomSchedules;
       return successResponse(req, res, data);
     } catch (error) {
       return errorResponse(req, res, error.message, 400, error);
