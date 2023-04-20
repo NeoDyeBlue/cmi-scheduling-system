2e371c9805faafadbf6ee3a85a65623631394e26
NeoDyeBlue <57549644+NeoDyeBlue@users.noreply.github.com>
2023-04-20 21:44:28 +0800

    Update sheet-validator.js

--- src/lib/validators/sheet-validator.js
+++ src/lib/validators/sheet-validator.js
@@ -3,18 +3,19 @@ import * as yup from 'yup';
 export const sheetSchema = yup.object().shape({
   file: yup
     .mixed()
-    .required('Please upload an Excel file')
     .test(
       'fileType',
       'Invalid file format. Only XLSX files are allowed.',
       (value) => {
-        if (!value) return false; // Skip validation if file is not present
+        if (!value) return false; // Skip validation if value is not present
+        if (typeof value !== 'string' || !value.startsWith('data:'))
+          return false; // Check if value is a valid data URL
+        const mimeType = value.split(';')[0].split(':')[1]; // Extract the mime type from the data URL
         return (
-          value &&
-          [
-            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-          ].includes(value.type)
-        );
+          mimeType ===
+          'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        ); // Check if the mime type is valid
       }
-    ),
+    )
+    .required('Please upload an Excel file'),
 });
